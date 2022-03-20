662bcc4f63181bc96c44c7956b1aea89f3b58608
Brendan <b-mierau@hotmail.com>
2022-03-20 15:00:52 -0700

    View Topic Screen activity implemented properly

---
+++ app/src/main/java/com/example/onlinevotingsystem/View_Topic_Screen.java
@@ -0,0 +1,14 @@
+package com.example.onlinevotingsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class View_Topic_Screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_topic_screen);
+    }
+}

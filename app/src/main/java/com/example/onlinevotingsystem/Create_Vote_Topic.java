68a61395e11db404e979588b5901c4048c74aa7a
Brendan <b-mierau@hotmail.com>
2022-04-02 10:45:53 -0700

    Create Vote Topic created and early pushed for Trevor to use and test the firebase implementation

---
+++ app/src/main/java/com/example/onlinevotingsystem/Create_Vote_Topic.java
@@ -0,0 +1,14 @@
+package com.example.onlinevotingsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Create_Vote_Topic extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_vote_topic);
+    }
+}

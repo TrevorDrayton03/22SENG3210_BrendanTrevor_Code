4d9cf9e6c8a671d8ebb058aec0efb5bc369c671c
Brendan <b-mierau@hotmail.com>
2022-04-02 15:14:42 -0700

    Layout of the Create_Vote_Topic is finished Linked the Manager_Dashboard to the Create_Vote_Topic activity through the create topic button

--- app/src/main/java/com/example/onlinevotingsystem/Create_Vote_Topic.java
+++ app/src/main/java/com/example/onlinevotingsystem/Create_Vote_Topic.java
@@ -2,13 +2,52 @@ package com.example.onlinevotingsystem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class Create_Vote_Topic extends AppCompatActivity {
+    DatePickerDialog.OnDateSetListener setListener;
+    TextView topicEndDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_vote_topic);
+
+        topicEndDate = findViewById(R.id.datePicker);
+
+        Calendar calendar = Calendar.getInstance();
+        final int year = calendar.get(Calendar.YEAR);
+        final int month = calendar.get(Calendar.MONTH);
+        final int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        topicEndDate.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(
+                        Create_Vote_Topic.this,android.R.style.Theme_Holo_Light_Dialog_MinWidth
+                        ,setListener,year,month,day);
+                datePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                datePickerDialog.show();
+            }
+        }));
+
+        setListener = new DatePickerDialog.OnDateSetListener(){
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                month = month++;
+                String date = day+"/"+month+"/"+year;
+                topicEndDate.setText(date);
+            }
+        };
     }
 }
